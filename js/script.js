e86171b53cc80f40726761249855c33cf29176e9
gatts123 <agattani@seas.upenn.edu>
2024-01-04 18:12:10 +0530

    mobile update

--- js/script.js
+++ js/script.js
@@ -1,46 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the education link and education div by their IDs
     var educationLink = document.getElementById('education-link');
     var educationDiv = document.getElementById('education');
   
-    // Add a click event listener to the education link
     educationLink.addEventListener('click', function (event) {
-      // Prevent the default behavior of the link (e.g., navigating to "#")
       event.preventDefault();
-  
-      // Scroll to the education div
       educationDiv.scrollIntoView({ behavior: 'smooth' });
     });
   });
 
 
   document.addEventListener('DOMContentLoaded', function () {
-    // Get the education link and education div by their IDs
-    var educationLink = document.getElementById('projects-link');
-    var educationDiv = document.getElementById('projects');
+    var projectsLink = document.getElementById('projects-link');
+    var projectsDiv = document.getElementById('projects');
   
-    // Add a click event listener to the education link
-    educationLink.addEventListener('click', function (event) {
-      // Prevent the default behavior of the link (e.g., navigating to "#")
+    projectsLink.addEventListener('click', function (event) {
       event.preventDefault();
-  
-      // Scroll to the education div
-      educationDiv.scrollIntoView({ behavior: 'smooth' });
+      projectsDiv.scrollIntoView({ behavior: 'smooth' });
     });
   });
 
   document.addEventListener('DOMContentLoaded', function () {
-    // Get the education link and education div by their IDs
-    var educationLink = document.getElementById('timeline-link');
-    var educationDiv = document.getElementById('timeline');
+    var timelineLink = document.getElementById('timeline-link');
+    var timelineDiv = document.getElementById('timeline');
   
-    // Add a click event listener to the education link
-    educationLink.addEventListener('click', function (event) {
-      // Prevent the default behavior of the link (e.g., navigating to "#")
+    timelineLink.addEventListener('click', function (event) {
       event.preventDefault();
-  
-      // Scroll to the education div
-      educationDiv.scrollIntoView({ behavior: 'smooth' });
+      timelineDiv.scrollIntoView({ behavior: 'smooth' });
     });
   });
   
