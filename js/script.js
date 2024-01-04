c7c4651dae80c4aadc23f32af2376160819502ae
gatts123 <agattani@seas.upenn.edu>
2024-01-04 17:24:22 +0530

    basic timeline added

--- js/script.js
+++ js/script.js
@@ -28,4 +28,19 @@ document.addEventListener('DOMContentLoaded', function () {
       educationDiv.scrollIntoView({ behavior: 'smooth' });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get the education link and education div by their IDs
+    var educationLink = document.getElementById('timeline-link');
+    var educationDiv = document.getElementById('timeline');
+  
+    // Add a click event listener to the education link
+    educationLink.addEventListener('click', function (event) {
+      // Prevent the default behavior of the link (e.g., navigating to "#")
+      event.preventDefault();
+  
+      // Scroll to the education div
+      educationDiv.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
   
